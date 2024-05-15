1ce4ba5150d90eed745bdd4a8b04bf651ea42deb
Otávio Ferreira <38397566+otavio-f@users.noreply.github.com>
2024-05-15 19:33:22 -0300

    Create 1004.c

---
+++ src/1004.c
@@ -0,0 +1,18 @@
+/*
+ * 1004 - Simple Product
+ * Author: Otavio
+ * Date: 25/04/2024
+ */
+#include <stdio.h>
+ 
+int
+main()
+{
+    int a, b;
+    
+    scanf("%d", &a);
+    scanf("%d", &b);
+    
+    printf("PROD = %d\n", a*b);
+    return 0;
+}
