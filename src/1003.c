48b29259092a09d1a1303d97d5f1232a6b0ebabe
Otávio Ferreira <38397566+otavio-f@users.noreply.github.com>
2024-05-15 19:30:11 -0300

    Create 1003.c

---
+++ src/1003.c
@@ -0,0 +1,18 @@
+/*
+ * 1003 - Simple Sum
+ * Author: Otavio
+ * Date: 25/04/2024
+ */
+#include <stdio.h>
+ 
+int
+main()
+{
+    int a, b;
+    
+    scanf("%d", &a);
+    scanf("%d", &b);
+    
+    printf("SOMA = %d\n", a+b);
+    return 0;
+}
