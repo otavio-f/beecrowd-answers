97d5981b60775167c2de9f4cb1499c8b690441be
Otávio Ferreira <38397566+otavio-f@users.noreply.github.com>
2024-05-15 19:24:42 -0300

    Create 1001.c

---
+++ src/1001.c
@@ -0,0 +1,16 @@
+/*
+ * Author: Otavio
+ * Date: 07/03/2024
+ */
+#include <stdio.h>
+ 
+int
+main()
+{
+    int a, b;
+    
+    scanf("%d", &a);
+    scanf("%d", &b);
+    printf("X = %d\n", a+b);
+    return 0;
+}
