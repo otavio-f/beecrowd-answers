532e49c7d8129141858daa52857b8b328f01d250
Otávio Ferreira <38397566+otavio-f@users.noreply.github.com>
2024-05-15 19:28:23 -0300

    Create 1002.c

---
+++ src/1002.c
@@ -0,0 +1,17 @@
+/*
+ * Author: Otavio
+ * Date: 25/04/2024
+ */
+#include <stdio.h>
+#define PI 3.14159
+
+int
+main()
+{
+    double R;
+    scanf("%lf", &R);
+    
+    double A=PI*R*R;
+    printf("A=%.4lf\n", A);
+    return 0;
+}
