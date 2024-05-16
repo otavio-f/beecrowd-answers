4c46215a91c407791d76c8180f93925964d313a9
Otávio Ferreira <38397566+otavio-f@users.noreply.github.com>
2024-05-16 14:07:04 -0300

    Create 1016.c

---
+++ src/C/1016.c
@@ -0,0 +1,17 @@
+/*
+ * 1016 - Distance
+ * Author: Otavio
+ * Date: 25/04/2024
+ */
+#include <stdio.h>
+ 
+int
+main()
+{
+    int vel;
+    scanf("%d", &vel);
+    
+    int tm = 60*vel/30;
+    printf("%d minutos\n", tm);
+    return 0;
+}
