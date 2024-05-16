555b6656a11d440c93f2be3bb719923f9c68e537
Otávio Ferreira <38397566+otavio-f@users.noreply.github.com>
2024-05-16 14:14:33 -0300

    Create 1035.c

---
+++ src/C/1035.c
@@ -0,0 +1,21 @@
+/*
+ * 1035 - Selection Test 1
+ * Author: Otavio
+ * Date: 26/04/2024
+ */
+#include <stdio.h>
+
+int
+main()
+{
+	int a, b, c, d;
+
+	scanf("%d %d %d %d", &a, &b, &c, &d);
+
+	if ((b>c) && (d>a) && (c+d)>(a+b) && (c>0 && d>0) && (a%2 == 0))
+		printf("Valores aceitos\n");
+	else
+		printf("Valores nao aceitos\n");
+
+	return 0;
+}
