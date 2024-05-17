c476047bdc16d0b26eb2c04a69bdddd020ef389a
Otávio Ferreira <38397566+otavio-f@users.noreply.github.com>
2024-05-17 10:38:14 -0300

    Create 1059.c

---
+++ src/C/1059.c
@@ -0,0 +1,14 @@
+/*
+ * 1059 - Even Numbers
+ * Author: Otavio
+ * Date: 07/03/2024
+ */
+#include <stdio.h>
+ 
+int
+main()
+{
+    for(int i=2; i<=100; i+=2)
+        printf("%d\n", i);
+    return 0;
+}
