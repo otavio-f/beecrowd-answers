e925d1781ebb4bbda2f1d87ae47aa55fe55b0294
Otávio Ferreira <38397566+otavio-f@users.noreply.github.com>
2024-08-31 09:06:22 -0300

    Create 1000.c

---
+++ src/js/1000.c
@@ -0,0 +1,9 @@
+/*
+ * 1000 - Hello World!
+ * Author: Otavio
+ * Date: 31/08/2024
+ */
+var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+var lines = input.split('\n');
+
+console.log('Hello World!');
