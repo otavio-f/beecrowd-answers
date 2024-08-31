91216280801b628b7504ef8bdd3487acf5d0306e
Otávio Ferreira <38397566+otavio-f@users.noreply.github.com>
2024-08-31 09:19:52 -0300

    Update 1001.js

--- src/js/1001.js
+++ src/js/1001.js
@@ -1,4 +1,12 @@
+/*
+ * 1001 - Extremely Basic
+ * Author: Otavio
+ * Date: 31/08/2024
+ */
 var input = require('fs').readFileSync('/dev/stdin', 'utf8');
 var lines = input.split('\n');
 
-console.log('Hello World!');
+const A = parseInt(lines[0]);
+const B = parseInt(lines[1]);
+
+console.log('X =', A+B);
